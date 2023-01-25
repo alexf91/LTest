e61eba76516b56d8a8d2c9aadeceb28e8769064c
Alexander Fasching <fasching.a91@gmail.com>
2023-01-25 01:43:01 +0100

    Start with macro for testcase

---
+++ LTest/DSL.lean
@@ -0,0 +1,122 @@
+--
+-- Copyright 2023 Alexander Fasching
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+import LTest.Runtime
+import Lean
+import Lean.Meta
+import Lean.Parser
+open Lean
+open Lean.Meta
+open Lean.Parser
+
+
+set_option relaxedAutoImplicit false
+
+namespace LTest
+
+/--
+  Basic syntax elements used for the DSL.
+-/
+syntax fixtureDependency := "(" ident ":" ident ")"
+
+
+/--
+  Declare a testcase.
+
+  The testcase declaration is transformed into a function that calls the appropriate fixture
+  code and returns a result.
+  The transformed function has type `IO TestResult`.
+
+  We abuse some of the syntax here, as the type of fixture arguments is really a structure
+  and not a Type. This makes testcase declarations look like function definitions.
+  A testcase has type `IO Unit`.
+
+  A testcase definition looks like this:
+  ```
+  testcase test_something requires (a : FixtureA) (b : FixtureB) := do
+    return
+  ```
+  The fixture part can be omitted if the testcase has no dependencies.
+-/
+scoped macro (name := testcaseDecl)
+  doc?:optional(docComment)                         -- Documentation
+  "testcase"                                        -- Command name
+  name:ident                                        -- Name of the testcase
+  fixtures?:optional("requires" fixtureDependency+) -- Fixture arguments
+  ":=" body:term : command => do
+    dbg_trace s!"name: {name.getId}"
+
+    -- Get fixture requirements as `(Name × Name)` tuples.
+    let fixtures :=  fixtures?.raw[1].getArgs.map fun arg =>
+      (arg[1].getId, arg[3].getId)
+    dbg_trace s!"fixtures: {fixtures}"
+
+    -- This only works for testcases without fixtures.
+    let stx ← `(
+      def $name : IO TestResult := do
+        let (out, err, result) ← @captureResult Unit
+          $body
+
+        let exception := match result with
+        | .result _    => none
+        | .exception s => some s
+
+        return {
+          stdout := out
+          stderr := err
+          exception := exception
+        }
+    )
+    return stx
+
+
+/--
+  Declare a fixture.
+
+  Fixtures are more complicated than testcases, because they include a state,
+  a setup function and a teardown function.
+  It looks more like a structure definition, but with optional dependencies on other
+  fixtures.
+
+  The following code defines a fixture `FixtureC` with a state type `σ` and a value type `α`.
+  It depends on `FixtureA` and `FixtureB`.
+
+  ```
+  fixture FixtureC σ α requires (a : FixtureA) (b : FixtureB) where
+    default  := (object of type σ)
+    setup    := do return (object of type α)
+    teardown := do return
+  ```
+-/
+scoped macro (name := fixtureDecl)
+  doc?:optional(docComment)                         -- Documentation
+  "fixture"                                         -- Command name
+  name:ident                                        -- Name of the fixture
+  state:ident                                       -- State type of the fixture
+  value:ident                                       -- Value type of the fixture
+  fixtures?:optional("requires" fixtureDependency+) -- Fixture arguments
+  "where"                                           -- Delimiter keyword
+  body:term : command => do
+    return TSyntax.mk mkNullNode
+
+
+fixture FooBar Nat Nat requires (a : FixtureA) (b : FixtureB) where
+  let a := 20
+  return
+
+
+
+end LTest
---
+++ LTest/Runtime.lean
@@ -0,0 +1,100 @@
+--
+-- Copyright 2023 Alexander Fasching
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+import Lean
+open Lean
+
+set_option relaxedAutoImplicit false
+
+namespace LTest
+
+
+/--
+  Fixture scope.
+
+  This determines when the fixture setup and teardown code is called:
+  • For `session`, the fixture is initialized when it is first used and deinitialized
+    after the last test.
+  • For `function`, the fixture is initialized and deinitialized for every testcase.
+-/
+inductive Scope where
+  | session
+  | function
+
+
+/--
+  Fixture with a state of type `σ` and a value of type `α`.
+
+  The state is updated by the `setup` and `teardown` functions, the value is passed
+  to a testcase or another fixture that depends on the fixture.
+-/
+structure Fixture (σ : Type) (α : Type) where
+  doc      : Option String := none
+  scope    : Scope := .function
+  default  : σ
+  setup    : StateT σ IO α
+  teardown : StateT σ IO Unit
+
+
+/--
+  Result of a testcase.
+
+  This includes the the result of the test itself and everything from
+  fixture setup and teardown
+-/
+structure TestResult where
+  stdout    : String
+  stderr    : String
+  exception : Option String
+  deriving Repr
+
+
+/--
+  Result type for captureResult to distinguish between exceptions and results.
+-/
+inductive ResultType (α : Type) where
+  | result (r : α)
+  | exception (msg : String)
+
+
+open ResultType in
+/--
+  Capture stdout, stderr and exceptions.
+
+  This is taken from withIsolatedStreams in Lean/System/IO.lean and
+  modified to split stdout and stderr.
+-/
+def captureResult (x : IO α) : IO (String × String × ResultType α) := do
+    let bIn  ← IO.mkRef { : IO.FS.Stream.Buffer }
+    let bOut ← IO.mkRef { : IO.FS.Stream.Buffer }
+    let bErr ← IO.mkRef { : IO.FS.Stream.Buffer }
+
+    let mut r := exception "unknown exception"
+    try
+      r := result (← IO.withStdin  (IO.FS.Stream.ofBuffer bIn)  <|
+                     IO.withStdout (IO.FS.Stream.ofBuffer bOut) <|
+                     IO.withStderr (IO.FS.Stream.ofBuffer bErr) x)
+    catch exc =>
+      r := exception exc.toString
+
+    let bOut ← liftM (m := BaseIO) bOut.get
+    let bErr ← liftM (m := BaseIO) bErr.get
+    let out := String.fromUTF8Unchecked bOut.data
+    let err := String.fromUTF8Unchecked bErr.data
+
+    return (out, err, r)
+
+end LTest
